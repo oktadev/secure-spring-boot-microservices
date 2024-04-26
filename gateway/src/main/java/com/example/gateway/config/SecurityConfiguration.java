83cff7fc4b38795f80eb5806cb346fa31dae70cc
Deepu <d4udts@gmail.com>
2024-04-26 21:47:26 +0200

    Chapter 5: Create controller, profile and configure OIDC

---
+++ gateway/src/main/java/com/example/gateway/config/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.example.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    public SecurityConfiguration(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
+    private LogoutSuccessHandler logoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler logoutSuccessHandler = new OidcClientInitiatedLogoutSuccessHandler(
+                this.clientRegistrationRepository);
+
+        // Sets the location that the end user's User Agent will be redirected to
+        // After the logout has been performed at the Provider
+        logoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
+
+        return logoutSuccessHandler;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2Login(withDefaults())
+                .logout(logout -> logout.logoutSuccessHandler(logoutSuccessHandler()));
+        return http.build();
+    }
+}
